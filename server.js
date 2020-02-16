7660f2b35504ca4fa8afcc30f7ea1b94ce366ca0
viriya <irwantojo@gmail.com>
2020-02-16 16:46:28 +0700

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 const bootcamps = require('./routes/bootcamps');
 
@@ -7,9 +8,14 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'));
+}
+
+
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,
-   console.log(`Server running in ${process.env.MODE_ENV} mode on port ${PORT}`))
+   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
